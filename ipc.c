7c8d643717025c9216ec4b9c3e39213572892b33
Anton Surkis <antonsurkis@gmail.com>
2021-11-04 11:59:55 +0300

    Add code from lab1

---
+++ ipc.c
@@ -0,0 +1,96 @@
+#include "pa23.h"
+
+static int write_repeat(int fd, char const *buf, size_t size) {
+  while (size) {
+    ssize_t shift = write(fd, buf, size);
+    CHK_ERRNO(shift);
+    if (!shift)
+      return -1;
+    buf += shift;
+    size -= shift;
+  }
+  return 1;
+}
+
+static int read_repeat(int fd, char *buf, size_t size) {
+  while (size) {
+    ssize_t shift = read(fd, buf, size);
+    CHK_ERRNO(shift);
+    if (!shift)
+      return -1;
+    buf += shift;
+    size -= shift;
+  }
+  return 1;
+}
+
+int send(void *self_, local_id dst_, Message const *msg) {
+  struct Self *self = self_;
+  int fd = self->pipes[2 * (self->id * self->n_processes + dst_) + 1];
+  int retcode = write_repeat(fd, (char *)&msg->s_header, sizeof(MessageHeader));
+  CHK_RETCODE(retcode);
+
+  switch (msg->s_header.s_type) {
+  case STARTED:
+  case DONE:
+    retcode = write_repeat(fd, msg->s_payload, msg->s_header.s_payload_len);
+    CHK_RETCODE(retcode);
+    break;
+
+  case ACK:
+  case STOP:
+    break;
+
+  case TRANSFER:
+  case BALANCE_HISTORY:
+  default:
+    return -1;
+  }
+
+  self->local_time++;
+  return 0;
+}
+
+int send_multicast(void *self_, Message const *msg) {
+  struct Self *self = self_;
+  for (size_t i = 0; i < self->n_processes; ++i) {
+    if (i != self->id) {
+      send(self, (local_id)i, msg);
+    }
+  }
+  return 0;
+}
+
+int receive(void *self_, local_id from, Message *msg) {
+  struct Self *self = self_;
+  int fd = self->pipes[2 * (from * self->n_processes + self->id)];
+  int retcode = read_repeat(fd, (char *)&msg->s_header, sizeof(MessageHeader));
+  CHK_RETCODE(retcode);
+
+  switch (msg->s_header.s_type) {
+  case STARTED:
+  case DONE:
+    retcode = read_repeat(fd, msg->s_payload, msg->s_header.s_payload_len);
+    CHK_RETCODE(retcode);
+    break;
+
+  case ACK:
+  case STOP:
+    break;
+
+  case TRANSFER:
+  case BALANCE_HISTORY:
+  default:
+    return -1;
+  }
+
+  self->local_time++;
+  return 0;
+}
+
+int receive_any(void *self_, Message *msg) {
+  struct Self *self = self_;
+  (void)self;
+  /* Unimplemented */
+  return -1;
+}
--- pa23.c
+++ pa23.c
@@ -1,15 +1,120 @@
+#include "pa23.h"
 #include "banking.h"
 
-void transfer(void * parent_data, local_id src, local_id dst,
-              balance_t amount)
-{
-    // student, please implement me
+void transfer(void *parent_data, local_id src, local_id dst, balance_t amount) {
+  // student, please implement me
 }
 
-int main(int argc, char * argv[])
-{
-    //bank_robbery(parent_data);
-    //print_history(all);
+static int init_process(struct Self *self) {
+  for (size_t i = 0; i < self->n_processes; ++i) {
+    for (size_t j = 0; j < self->n_processes; ++j) {
+      if (i == j)
+        continue;
+      int *pair = &self->pipes[2 * (i * self->n_processes + j)];
+      if (j != self->id) {
+        fprintf(self->pipes_log, "Process %zu closing its pipe end %d\n",
+                self->id, pair[0]);
+        CHK_ERRNO(close(pair[0]));
+      }
+      if (i != self->id) {
+        fprintf(self->pipes_log, "Process %zu closing its pipe end %d\n",
+                self->id, pair[1]);
+        CHK_ERRNO(close(pair[1]));
+      }
+    }
+  }
+  return 0;
+}
 
+static int deinit_process(struct Self *self) {
+  for (size_t i = 0; i < self->n_processes; ++i) {
+    if (i != self->id) {
+      int read_end = self->pipes[2 * (i * self->n_processes + self->id) + 0];
+      int write_end = self->pipes[2 * (self->id * self->n_processes + i) + 1];
+      fprintf(self->pipes_log, "Process %zu closing its pipe end %d\n",
+              self->id, read_end);
+      CHK_ERRNO(close(read_end));
+      fprintf(self->pipes_log, "Process %zu closing its pipe end %d\n",
+              self->id, write_end);
+      CHK_ERRNO(close(write_end));
+    }
+  }
+  free(self->pipes);
+  fclose(self->pipes_log);
+  fclose(self->events_log);
+  return 0;
+}
+
+static int wait_for_message(struct Self *self, size_t from, Message *msg,
+                            MessageType type) {
+  do {
+    CHK_RETCODE(receive(self, (local_id)from, msg));
+  } while (msg->s_header.s_type != type);
+  return 0;
+}
+
+static int run_child(struct Self *self) {
+  Message msg;
+  CHK_RETCODE(init_process(self));
+  CHK_RETCODE(deinit_process(self));
+  return 0;
+}
+
+static int run_parent(struct Self *self) {
+  Message msg;
+  CHK_RETCODE(init_process(self));
+
+  // bank_robbery(parent_data);
+  // print_history(all);
+
+  for (size_t i = 1; i < self->n_processes; ++i)
+    wait(NULL);
+
+  CHK_RETCODE(deinit_process(self));
+  return 0;
+}
+
+struct Self self;
+
+int main(int argc, char *argv[]) {
+  size_t n_children;
+  if (argc < 4 || sscanf(argv[2], "%zu", &n_children) != 1) {
+    printf("Usage: %s -p <number of processes>"
+           "<amount of money for each process>\n",
+           argv[0]);
     return 0;
+  }
+
+  self.pipes_log = fopen(pipes_log, "w");
+  self.events_log = fopen(events_log, "a");
+
+  self.n_processes = n_children + 1;
+  self.pipes = malloc(2 * sizeof(int) * self.n_processes * self.n_processes);
+  for (size_t i = 0; i < self.n_processes; ++i) {
+    for (size_t j = 0; j < self.n_processes; ++j) {
+      int *to_create = &self.pipes[2 * (i * self.n_processes + j)];
+      if (i == j) {
+        to_create[0] = -1;
+        to_create[1] = -1;
+      } else {
+        CHK_ERRNO(pipe(to_create));
+        fprintf(self.pipes_log, "Pipe %zu ==> %zu: rfd=%d, wfd=%d\n", i, j,
+                to_create[0], to_create[1]);
+      }
+    }
+  }
+
+  self.local_time = 0;
+
+  for (size_t id = 1; id < self.n_processes; ++id) {
+    pid_t pid = fork();
+    CHK_ERRNO(pid);
+    if (!pid) {
+      self.id = id;
+      return run_child(&self);
+    }
+  }
+
+  self.id = 0;
+  return run_parent(&self);
 }
